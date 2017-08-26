5987a794fb8654b2fcf860d34180bb416c3ff506
Nova <noah.trauben@gmail.com>
2017-08-25 20:40:47 -0400

    starting stuff

---
+++ mainmodules/screen.py
@@ -0,0 +1,10 @@
+"""
+This is the unchanged stuff that the terminal will show. This will always show.
+I am feeling a black boarder around the edge. Not sure how the terminal will refresh, but its a start
+"""
+
+def screen():
+    test = "bhahjd"
+    return test
+def t():
+    return 1231
---
+++ testing.py
@@ -0,0 +1,7 @@
+#testing file
+
+
+from mainmodules import screen as screanclass
+
+print screanclass.screen()
+print screanclass.t()
