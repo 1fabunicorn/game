78989e81b02d0debc9173855ff6576e5e47c5bbc
Nova <noah.trauben@gmail.com>
2017-09-07 12:06:53 -0400

    brokenish read/right cyphertext function in ignorethis.py

--- mainmodules/ignorethis.py
+++ mainmodules/ignorethis.py
@@ -1,5 +1,11 @@
 """
+Nova Trauben
 vigenere cipher
+
+Notes:
+    It seems this function is relatively efficient, as timeit says,
+    10000 loops, best of 3: 59.4 usec per loop
+    returns a list of encrypted values, and plaintext values. Not sure where to go from their.
 """
 
 
@@ -18,4 +24,18 @@ def decrypt(cyphertext, key):
 
     for keyindex, numbers in enumerate(cyphertext):
         plaintext.append(chr(numbers - ord(key[keyindex % len_of_key]) % 255))
-    return plaintext
+    return "".join(plaintext)
+
+
+def write_plaintext(cyphertext, key, file_to_create, ):
+    pass
+
+    file = open(cyphertext, 'r')
+    file_name = file.readline()
+    file_name += ".txt"
+
+    for words in cyphertext:
+        file_to_create = open(file_to_create, 'a')
+        file_to_create.write(decrypt(cyphertext, key))
+
+    print(file_name)
