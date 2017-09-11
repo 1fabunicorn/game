670095c04da4dfcd7a1f05d82eb69047eaf40709
Nova <noah.trauben@gmail.com>
2017-09-11 12:12:18 -0400

    added documentation for builtins

--- mainmodules/CommandPrompt.py
+++ mainmodules/CommandPrompt.py
@@ -57,8 +57,28 @@ class HelloWorld(cmdcopy.Cmd):
         pass
 
     def do_ls(self, args):
+        '''
 
+        list files and directory's.
+        If no args are specified, ls will print in alphabetical order
+        for extended help, specify --help
 
+        optional args
+
+            -a  --all
+              do not ignore entries starting with .
+            -b  --escape
+              print C-style escapes for nongraphic characters
+            -h  --human-readable
+              with -l and/or -s, print human readable sizes (e.g., 1K 234M
+              2G)
+            -I  --ignore=PATTERN
+              do not list implied entries matching shell PATTERN
+            -l
+              use a long listing format
+            -1
+              list one file per line.  Avoid '\n' with -q or -b
+        '''
         try:
             subprocess.check_call(['ls', args])
         except:
@@ -68,16 +88,41 @@ class HelloWorld(cmdcopy.Cmd):
 
 
     def do_cat(self, file):
+        '''
+        cat - print files on the standard output
+        for extended help, specify --help
+        'cat [file_to_cat]'
+
+        optional args
+
+        -n, --number
+          number all output lines
+        -E, --show-ends
+          display $ at end of each line
+        '''
+
         try:
             subprocess.check_call(['cat', file])
         except:
             pass
 
     def do_clear(self, nothing):
+        '''
+        clear - clear the terminal screen
+
+        '''
+
         subprocess.call(['clear'])
 
     def do_nano(self, file):
+        '''
+        nano - friendly text editor
+        'nano [file_to_edit_or_create]'
 
+        if [file] is omitted, a temporary file will be created
+
+        '''
+        # hacker proofing
         if os.path.split(os.getcwd())[1] != os.path.split(file)[0] and file[0] == '/':
             print('not a directory')
             return
@@ -92,6 +137,14 @@ class HelloWorld(cmdcopy.Cmd):
         os.system(osCommandString)
 
     def do_vi(self, file):
+        '''
+        nano - a less friendly text editor
+        'nano [file_to_edit_or_create]'
+
+        if [file] is omitted, a temporary file will be created
+
+        '''
+        # hacker proofing
         if os.path.split(os.getcwd())[1] != os.path.split(file)[0] and file[0] == '/':
             print('not a directory')
             return
@@ -103,7 +156,15 @@ class HelloWorld(cmdcopy.Cmd):
         os.system(osCommandString)
 
     def do_touch(self, file):
-        subprocess.call(['touch', file])
+        '''
+        touch - "touch" or create a new file
+        if file already exist, add a new t
+
+        '''
+        try:
+            subprocess.call(['touch', file])
+        except:
+            print("file not specified")
 
 
 if __name__ == 'mainmodules.CommandPrompt': # it works with this!!!
