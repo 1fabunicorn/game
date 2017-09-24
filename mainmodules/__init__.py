e82998e98055cea9e85315274b550ebe57783f47
Nova <noah.trauben@gmail.com>
2017-09-24 16:15:24 -0400

    Branch without /unittesting

--- mainmodules/__init__.py
+++ mainmodules/__init__.py
@@ -1,1 +1,1 @@
-__all__ = ['border', 'cmdcopy', 'CommandPrompt', 'DotDotDot', 'ignorethis', 'TermUtil', 'checks', 'tasks']
+__all__ = ['border', 'cmdcopy', 'CommandPrompt', 'ignorethis', 'TermUtil', 'checks', 'tasks']
